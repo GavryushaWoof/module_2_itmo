0b417de54f4f8240613cff36826661495118f8b5
GavryushaWoof <gavgav.cat@yandex.ru>
2019-11-16 01:39:07 +0300

    homework2 4/6

---
+++ homework2/js/script.js
@@ -0,0 +1,89 @@
+//Задача 1
+let month = +prompt('Введите номер месяца', '1');
+switch (true) {
+    case month >= 1 && month <= 3:
+        alert('Зима');
+        break;
+    case month >= 4 && month <= 6:
+        alert('Весна');
+        break;
+    case month >= 7 && month <= 9:
+        alert('Лето');
+        break;
+    case month >= 10 && month <= 12:
+        alert('Осень');
+        break;
+    default:
+        alert('Некорректно введен номер месяца');
+}
+
+//Задача 2
+let measure = +prompt('Выберете меру измерения: \n 1. Километр \n 2. Метр \n 3. Дециметр \n 4. Сантиметр \n 5. Миллиметр', '2');
+let lenght = +prompt('Введите длину', '0');
+switch (measure) {
+    case 1:
+        alert('Ваше значение ' + (lenght * 1000) + ' м');
+        break;
+    case 2:
+        alert('Ваше значение ' + lenght + ' м');
+        break;
+    case 3:
+        alert('Ваше значение ' + (lenght / 10) + ' м');
+        break;
+    case 4:
+        alert('Ваше значение ' + (lenght / 100) + ' м');
+        break;
+    case 5:
+        alert('Ваше значение ' + (lenght / 1000) + ' м');
+        break;
+
+}
+
+//задача 3
+let num = +prompt('Введите число', '0');
+switch (true) {
+    case num === 0:
+        alert('Нулевое число');
+        break;
+    case num > 0 && num < 10:
+        alert('Положительное однозначное число');
+        break;
+    case num > 9 && num < 100:
+        alert('Положительное двузначное число');
+        break;
+    case num > 99 && num < 1000:
+        alert('Положительное трехзначное число');
+        break;
+    case num < 0 && num > -10:
+        alert('Отрицательное однозначное число');
+        break;
+    case num < -9 && num > -100:
+        alert('Отрицательное двузначное число');
+        break;
+    case num < -99 && num > -1000:
+        alert('Отрицательное трехзначное число');
+        break;
+}
+
+//Задача 4
+for (let i = 1; i <= 100; i++) {
+    switch (true) {
+        case i % 3 == 0 && i % 5 == 0:
+            console.log('ThreeFive');
+            break;
+        case i % 3 == 0:
+            console.log('Three');
+            break;
+        case i % 5 == 0:
+            console.log('Five');
+            break;
+        default:
+            console.log(i);
+    }
+}
+
+//Задача 5
+
+
+
+//задача 6
