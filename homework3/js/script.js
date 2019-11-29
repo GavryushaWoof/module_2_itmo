959048ea184f528830240da7eb2ce9520a5e178d
GavryushaWoof <gavgav.cat@yandex.ru>
2019-11-30 00:28:51 +0300

    homework3 ex2

--- homework3/js/script.js
+++ homework3/js/script.js
@@ -6,9 +6,24 @@ console.log(C);
 C = B.concat(A);
 console.log(C);
 //Задача 2
-
-
-//задача 3
+let area = [null, null, null, null, null, null, null, null, null];
+for (let i = 0; i < area.length; i++) {
+    sim = prompt('Введите 0 для нолика или 1 для крестика');
+    if (sim === '0') {
+        area[i] = '<td>0</td>';
+    } else if (sim === '1') {
+        area[i] = '<td>X</td>';
+    } else {
+        area[i] = '<td></td>';
+    }
+    if (i == 0 || i == 3 || i == 6) {
+        area[i] = `<tr>${area[i]}`;
+    } else if (i == 2 || i == 5 || i == 8) {
+        area[i] = `${area[i]}</tr>`;
+    }
+}
+document.write(`<table>${area.join('')}</table>`)
+    //задача 3
 let G = [12, 4, 3, 10, 1, 20],
     min = G[0],
     max = G[0],
@@ -29,8 +44,4 @@ for (let i = 1; i < G.length; i++) {
         console.log(G);
     }
 }
-
-
-
-
 //Задача 4
