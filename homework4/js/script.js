f8f9cb5f65d557ab28979ee919ecb299219874cd
GavryushaWoof <gavgav.cat@yandex.ru>
2019-12-09 21:40:40 +0300

    homework4 fix ex 1

--- homework4/js/script.js
+++ homework4/js/script.js
@@ -1,23 +1,26 @@
 //Задача 1
 let str = prompt('Введите число', '');
-let rez = +str + +str;
+let rez = 0;
+for (let i = 0; i < str.length; i++) {
+    rez += +str[i];
+}
 if (!isNaN(rez)) {
     console.log(rez);
 } else {
     console.log('Введено не число');
 }
 //Задача 2
-let str2 = prompt('Введите слово', '');
-let sim = prompt('Введите символ', '');
-let reg = new RegExp(sim, 'ig');
-console.log(str2.replace(reg, sim + sim));
+// let str2 = prompt('Введите слово', '');
+// let sim = prompt('Введите символ', '');
+// let reg = new RegExp(sim, 'ig');
+// console.log(str2.replace(reg, sim + sim));
 
-//задача 3
-let password = prompt('Введите пароль', '');
-let reg = /(.*[a-z][A-Z])+(.*\d){2,}(.*\!|\$|\#|\%)+/;
-if (reg.test(password) && password.length >= 9) {
-    console.log('Верный пароль');
-} else {
-    console.log('Неверный пароль');
-}
+// //задача 3
+// let password = prompt('Введите пароль', '');
+// let reg2 = /(.*[a-z][A-Z])+(.*\d){2,}(.*\!|\$|\#|\%)+/;
+// if (reg2.test(password) && password.length >= 9) {
+//     console.log('Верный пароль');
+// } else {
+//     console.log('Неверный пароль');
+// }
 //Задача 4
